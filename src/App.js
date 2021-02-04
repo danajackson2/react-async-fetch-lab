aa28d68833c34aa9833e8fd494c175990e7207bf
danajackson2 <danajackson2@gmail.com>
2021-02-03 21:01:20 -0800

    best API

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+    state={
+        astros: []
+    }
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                astros: data.people
+            })
+        })
+    }
+
+    render(){
+        return(
+            <ol>
+                {this.state.astros.map(p =>  <li>{p.name}</li>)}
+            </ol>
+        )
+    }
+
+}
+
+export default App
